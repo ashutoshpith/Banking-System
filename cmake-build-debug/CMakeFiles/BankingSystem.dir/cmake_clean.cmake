ede53bedf34d0d1ddf1c85244a8a46c7bcfe397e
ashutoshpith <amxdp18@gmail.com>
2019-01-31 02:03:27 +0530

    Banking system

---
+++ cmake-build-debug/CMakeFiles/BankingSystem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BankingSystem.dir/main.cpp.obj"
+  "BankingSystem.pdb"
+  "BankingSystem.exe"
+  "BankingSystem.exe.manifest"
+  "libBankingSystem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BankingSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
